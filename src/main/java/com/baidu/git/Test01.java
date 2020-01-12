2fe8a07bc38895d863594732b6c66cba9ad260c8
zhangsan <zhangsan@163.com>
2020-01-12 22:50:19 +0800

    edit by lishiwei

---
+++ src/main/java/com/baidu/git/Test01.java
@@ -0,0 +1,14 @@
+package com.baidu.git;
+
+/**
+ * <p>NAME: Test01</p>
+ *
+ * @author Bryce
+ * @version 1.0
+ * @date 2020-01-12 22:49:43
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01");
+    }
+}
