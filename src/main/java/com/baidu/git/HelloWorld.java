81f55475722b194db64e125a8db652f50445b94b
zhangsan <zhangsan@163.com>
2020-01-12 22:21:43 +0800

    init git-01-test

---
+++ src/main/java/com/baidu/git/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.baidu.git;
+
+/**
+ * <p>NAME: HelloWorld</p>
+ *
+ * @author Bryce
+ * @version 1.0
+ * @date 2020-01-12 21:56:47
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好：Hello World世界");
+    }
+}
